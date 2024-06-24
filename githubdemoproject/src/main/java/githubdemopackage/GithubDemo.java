fc372e10066891bd3fc9d82fcf8b55602d2895e6
Lenovo <Lenovo@DESKTOP-LOTSCU6>
2024-06-24 21:20:12 +0530

    githubdemo

---
+++ githubdemoproject/src/main/java/githubdemopackage/GithubDemo.java
@@ -0,0 +1,12 @@
+package githubdemopackage;
+
+public class GithubDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Github Demo");
+
+	}
+
+}
